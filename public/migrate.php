83a6b8a044c0341561204d5cabafe15b8c5ee036
Cornélio <cornelio@example.com>
2026-06-12 16:32:23 +0000

    fix: correct paths in migrate.php for public folder

--- public/migrate.php
+++ public/migrate.php
@@ -6,8 +6,8 @@
  * IMPORTANTE: Delete este arquivo imediatamente após o uso!
  */
 
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
